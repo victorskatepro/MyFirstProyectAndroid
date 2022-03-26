466ae598130177d74b818034089a45aa1ddf7036
Victor Saico <victor.saico@pedidosya.com>
2022-03-26 11:41:12 -0500

    add Carview

---
+++ app/src/main/java/com/saico/myfirstproyectandroid/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.saico.myfirstproyectandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.AlarmClock
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "message")
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({ startActivity(intent); finish() },3000)
+    }
+}
